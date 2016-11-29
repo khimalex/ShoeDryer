5890ab311ce57070bb078bb59c89a69f927b1e9b
khimalex <khimalex@gmail.com>
2016-11-29 11:05:43 +0300

    Optimizing Task Cancellation

--- ShoeDryer/MainWindowVM.cs
+++ ShoeDryer/MainWindowVM.cs
@@ -14,49 +14,54 @@ namespace ShoeDryer
 
 		public MainWindowVM()
 		{
-			StartCommand = AsyncCommandFactory.Create(StartCommandAsync, t => Threads > 0 && Tasks.Count == 0);
+			StartCommand = AsyncCommandFactory.Create(StartCommandAsync, t => Threads > 0 && TasksContinues.Count == 0);
 			StopCommand = AsyncCommandFactory.Create(StopCommandAsync, t => Cancel != null);
 		}
 		public Int32 Threads { get; set; }
 		private List<Task> _Tasks;
-		private List<Task> Tasks => _Tasks ?? (_Tasks = new List<Task>());
+		private List<Task> TasksContinues => _Tasks ?? (_Tasks = new List<Task>());
 
 		public IAsyncCommand StartCommand { get; set; }
 		private CancellationTokenSource Cancel { get; set; }
 		private async Task StartCommandAsync()
 		{
-			if (Cancel != null && Tasks.Count != 0)
+			if (Cancel != null && TasksContinues.Count != 0)
 			{
 				await StopCommand.ExecuteAsync(null);
 			}
 			Cancel = new CancellationTokenSource();
 			for (int i = 0; i < Threads; i++)
 			{
-				var task = WorkTask();
-				Tasks.Add(task);
+
+				//var task = WorkTask(Cancel.Token);
+				//try/catch instead - using continuewith OnlyOnCancelled
+				var taskContinue = WorkTask(Cancel.Token).ContinueWith(t => TasksContinues.Clear(), TaskContinuationOptions.OnlyOnCanceled);
+				TasksContinues.Add(taskContinue);
 			}
-			await Task.WhenAll(Tasks);
+			await Task.WhenAll(TasksContinues).ConfigureAwait(false);
 		}
 		public IAsyncCommand StopCommand { get; set; }
 		private async Task StopCommandAsync()
 		{
 			Cancel?.Cancel();
 			Cancel = null;
-			Tasks.Clear();
 		}
 
-		private async Task WorkTask()
+		private Task WorkTask(CancellationToken token)
 		{
-			await Task.Run(() =>
+			return Task.Run(() =>
 			{
 				var random = new Random();
-				while ((Cancel?.Token.IsCancellationRequested).HasValue && !Cancel.Token.IsCancellationRequested)
+				while (true)
 				{
 					random.Next(Int32.MinValue, Int32.MaxValue);
+					//In any cases even if i call "throw new OperationCancelException" i'll get an task status == cancelled.
+					//Special exception type are important.
+					//But we'll use a classic approach. When we didn't have async/await keywords.
+					if (token.IsCancellationRequested)
+						token.ThrowIfCancellationRequested();
 				}
-				//Possibly it not need throwing. Because we exited from while-loop and task wil be finished.
-				//Cancel?.Token.ThrowIfCancellationRequested();
-			}, Cancel.Token);
+			});
 		}
 
 		#region INotifyPropertyChanged
