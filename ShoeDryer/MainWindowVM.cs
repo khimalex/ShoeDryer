dc5ae8c59e04adda5b15b097cfe27d0968bc64f4
khimalex <khimalex@gmail.com>
2017-02-10 15:28:20 +0300

    Replace thread selector co ComboBox focused by default.

--- ShoeDryer/MainWindowVM.cs
+++ ShoeDryer/MainWindowVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -9,20 +10,44 @@ using System.Threading.Tasks;
 
 namespace ShoeDryer
 {
-	public class MainWindowVM : INotifyPropertyChanged
+	public class MainWindowVM: INotifyPropertyChanged
 	{
 
 		public MainWindowVM()
 		{
-			StartCommand = AsyncCommandFactory.Create(StartCommandAsync, t => Threads > 0 && TasksContinues.Count == 0);
+			SetCoreThreads();
+			StartCommand = AsyncCommandFactory.Create(StartCommandAsync, t => CoresForWorking > 0 && TasksContinues.Count == 0);
 			StopCommand = AsyncCommandFactory.Create(StopCommandAsync, t => Cancel != null);
+
 		}
-		public Int32 Threads { get; set; }
+
+		public ObservableCollection<Int32> CoresDropDown
+		{
+			get; set;
+		} = new ObservableCollection<int>();
+
+		private Int32 _CoresForExecuting = 1;
+		public Int32 CoresForWorking
+		{
+			get
+			{
+				return _CoresForExecuting;
+			}
+			set
+			{
+				SetField(ref _CoresForExecuting, value);
+			}
+		}
 		private List<Task> _Tasks;
 		private List<Task> TasksContinues => _Tasks ?? (_Tasks = new List<Task>());
-
-		public IAsyncCommand StartCommand { get; set; }
-		private CancellationTokenSource Cancel { get; set; }
+		public IAsyncCommand StartCommand
+		{
+			get; set;
+		}
+		private CancellationTokenSource Cancel
+		{
+			get; set;
+		}
 		private async Task StartCommandAsync()
 		{
 			if (Cancel != null && TasksContinues.Count != 0)
@@ -30,9 +55,8 @@ namespace ShoeDryer
 				await StopCommand.ExecuteAsync(null);
 			}
 			Cancel = new CancellationTokenSource();
-			for (int i = 0; i < Threads; i++)
+			for (int i = 0; i < CoresForWorking; i++)
 			{
-
 				//var task = WorkTask(Cancel.Token);
 				//try/catch instead - using continuewith OnlyOnCancelled
 				var taskContinue = WorkTask(Cancel.Token).ContinueWith(t => TasksContinues.Clear(), TaskContinuationOptions.OnlyOnCanceled);
@@ -40,7 +64,10 @@ namespace ShoeDryer
 			}
 			await Task.WhenAll(TasksContinues).ConfigureAwait(false);
 		}
-		public IAsyncCommand StopCommand { get; set; }
+		public IAsyncCommand StopCommand
+		{
+			get; set;
+		}
 		private async Task StopCommandAsync()
 		{
 			Cancel?.Cancel();
@@ -64,6 +91,13 @@ namespace ShoeDryer
 			}, token);
 		}
 
+		private void SetCoreThreads()
+		{
+			for (int i = 1; i <= Environment.ProcessorCount; i++)
+			{
+				CoresDropDown.Add(i);
+			}
+		}
 		#region INotifyPropertyChanged
 
 		public event PropertyChangedEventHandler PropertyChanged;
